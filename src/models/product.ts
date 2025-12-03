819ed180ed960d467030d5ce6ad1eac78d92e9da
matos795 <matosalexandre795@gmail.com>
2025-12-03 16:33:29 -0300

    Passando um Objeto como Props

---
+++ src/models/product.ts
@@ -0,0 +1,10 @@
+import type { CategoryDTO } from "./category";
+
+export type ProductDTO = {
+    id: number;
+    name: string;
+    description: string;
+    imgUrl: string;
+    price: number;
+    categories: CategoryDTO[];
+}
--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -4,23 +4,45 @@ import ButtonPrimary from '../../components/ButtonPrimary';
 import ButtonInverse from '../../components/ButtonInverse';
 import ProductDetailsCard from '../../components/ProductDetailsCard';
 import HeaderClient from '../../components/HeaderClient';
+import type { ProductDTO } from '../../models/product';
 
+const product: ProductDTO = {
+  id: 2,
+  name: "smart tv",
+  description: "é muito bonita",
+  imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/refs/heads/master/backend/img/2-big.jpg",
+  price: 2500.99,
+  categories: [
+    {
+      id: 2,
+      name: "Eletronicos"
+    },
+    {
+      id: 3,
+      name: "Computadores"
+    },
+    {
+      id: 4,
+      name: "Importados"
+    }
+  ]
+}
 
 export default function ProductDetails() {
 
-    return (
-        <>
-          <HeaderClient />
-          <main>
-            <section id="product-container-section" className="dsc-container">
-              <ProductDetailsCard />
-              <div className="dsc-btn-page-container">
-                <ButtonPrimary />
-                <ButtonInverse />
-              </div>
-            </section>
-          </main>
-        </>
-      );
+  return (
+    <>
+      <HeaderClient />
+      <main>
+        <section id="product-container-section" className="dsc-container">
+          <ProductDetailsCard product={product}/>
+          <div className="dsc-btn-page-container">
+            <ButtonPrimary />
+            <ButtonInverse />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 
 }
