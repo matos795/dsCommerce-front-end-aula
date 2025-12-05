457f70a8665aa01540660ee9b290139b46887c1d
matos795 <matosalexandre795@gmail.com>
2025-12-05 11:40:17 -0300

    Adding Routes-Links

--- src/components/HeaderClient/index.tsx
+++ src/components/HeaderClient/index.tsx
@@ -1,19 +1,26 @@
 import './style.css'
 import cartIcon from '../../assets/cart.svg';
+import { Link } from 'react-router-dom';
 
 export default function HeaderClient() {
 
     return (
         <header className="dsc-header-client">
             <nav className="dsc-container">
-                <h1>DSCommerce</h1>
+                <Link to='/'>
+                    <h1>DSCommerce</h1>
+                </Link>
                 <div className="dsc-nav-right">
                     <div className="dsc-menu-items-container">
                         <div className="dsc-menu-item">
-                            <img src={cartIcon} alt="Carrinho de Compras" />
+                            <Link to="/cart">
+                                <img src={cartIcon} alt="Carrinho de Compras" />                        
+                            </Link>
                         </div>
                     </div>
-                    <a href="#">Entrar</a>
+                    <Link to="/login">
+                        <a href="#">Entrar</a>                  
+                    </Link>
                 </div>
             </nav>
         </header>
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -4,7 +4,7 @@ import ButtonPrimary from '../../../components/ButtonPrimary';
 import ButtonInverse from '../../../components/ButtonInverse';
 import ProductDetailsCard from '../../../components/ProductDetailsCard';
 import * as productService from '../../../services/product-service';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 
 export default function ProductDetails() {
@@ -22,7 +22,9 @@ export default function ProductDetails() {
         }
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
-          <ButtonInverse text="Início" />
+          <Link to="/">
+            <ButtonInverse text="Início" />        
+          </Link>
         </div>
       </section>
     </main>
