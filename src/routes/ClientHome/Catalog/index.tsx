54be6ff333f086742e47a367077bc9b2c59875f4
matos795 <matosalexandre795@gmail.com>
2025-12-12 19:42:11 -0300

    Update backend request

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -4,14 +4,14 @@ import CatalogCard from '../../../components/CatalogCard';
 import ButtomNextPage from '../../../components/ButtomNextPage';
 import { useEffect, useState } from 'react';
 import type { ProductDTO } from '../../../models/product';
-import axios from 'axios';
+import * as productService from '../../../services/product-service'
 
 export default function Catalog() {
 
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/products?size=12")
+    productService.findAll()
     .then(response => {
       setProducts(response.data.content);
     })
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -6,8 +6,7 @@ import ProductDetailsCard from '../../../components/ProductDetailsCard';
 import { Link, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import type { ProductDTO } from '../../../models/product';
-import axios from 'axios'
-
+import * as productService from '../../../services/product-service'
 
 export default function ProductDetails() {
 
@@ -17,7 +16,7 @@ export default function ProductDetails() {
 
   useEffect(() => {
 
-    axios.get(`http://localhost:8080/products/${params.productId}`)
+      productService.findById(Number(params.productId))
       .then(response => {
         console.log(response.data);
 
