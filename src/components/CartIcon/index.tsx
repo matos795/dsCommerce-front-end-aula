40dfac3e3be4435d7747919c424435df047f2889
matos795 <matosalexandre795@gmail.com>
2026-07-11 22:31:19 -0300

    feat: LoginForm and LoginRequest

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react"
+import { useContext } from "react"
 import cartIcon from "../../assets/cart.svg"
 import "./styles.css"
 import { ContextCartCount } from "../../utils/context-cart";
